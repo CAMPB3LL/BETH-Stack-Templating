2947943947d39c57afb9c46496c68a3ee8207dc9
CAMPB3LL <campbell.ohlsen@gmail.com>
2023-11-05 17:38:35 +0930

    Icons

--- index.tsx
+++ index.tsx
@@ -8,13 +8,13 @@ const app = new Elysia()
 		html(
 			<BaseHtml>
 				<body class="flex w-full h-screen justify-center items-center">
-					<div class="justify-items-center grid grid-cols-2 gap-x-10 gap-y-40">
-						<p class="font-sans text-7xl text-slate-300 font-bold col-start-1 col-span-2">Who are you?</p>
+					<div class="w-full justify-items-center grid grid-cols-2 gap-y-40 mx-20">
+						<p class="font-sans text-8xl text-slate-300 font-bold col-start-1 col-span-2">Who are you?</p>
 						<button hx-post="/clicked" hx-swap="outerHTML" class="bg-purple-600 h-48 w-80 rounded-full font-sans text-6xl text-slate-300 font-bold col-start-1 col-span-1">
-							USER
+							<i class="fa-solid fa-user"></i> USER
 						</button>
 						<button hx-post="/clicked" hx-swap="outerHTML" class="bg-purple-700 h-48 w-80 rounded-full font-sans text-6xl text-slate-300 font-bold col-start-2 col-span-2">
-							TECH
+						<i class="fa-solid fa-wrench"></i> TECH
 						</button>
 					</div>
 				</body>
@@ -35,6 +35,7 @@ const BaseHtml = ({children}: elements.Children) =>
 <title>TechSwift - On Domand IT</title>
 <script src="https://unpkg.com/htmx.org@1.9.7"></script>
 <script src="https://cdn.tailwindcss.com"></script>
+<script src="https://kit.fontawesome.com/0902ff1ba2.js" crossorigin="anonymous"></script>
 </head>
 ${children}`
 ;
