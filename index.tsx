22f36905d51790c07d58f633afc46c0ad7227513
CAMPB3LL <campbell.ohlsen@gmail.com>
2023-11-06 18:24:15 +0930

    Auto stash before merge of "main" and "origin/main"

--- index.tsx
+++ index.tsx
@@ -9,12 +9,12 @@ const app = new Elysia()
 			<BaseHtml>
 				<body class="flex w-full h-screen justify-center items-center">
 					<div class="w-full justify-items-center grid grid-cols-2 gap-y-40 mx-20">
-						<p class="font-sans text-8xl text-slate-300 font-bold col-start-1 col-span-2">Who are you?</p>
-						<button hx-post="/clicked" hx-swap="outerHTML" class="bg-purple-600 h-48 w-80 rounded-full font-sans text-6xl text-slate-300 font-bold col-start-1 col-span-1">
-							<i class="fa-solid fa-user"></i> USER
+						<p class="font-sans text-6xl text-slate-300 font-bold col-start-1 col-span-2">Who are you?</p>
+						<button hx-post="/clicked" hx-swap="outerHTML" class="bg-purple-600 h-max w-4/5 rounded-full font-sans text-4xl text-slate-300 font-bold col-start-1 col-span-1">
+							<p class="my-10"><i class="fa-solid fa-user"></i>USER</p>
 						</button>
-						<button hx-post="/clicked" hx-swap="outerHTML" class="bg-purple-700 h-48 w-80 rounded-full font-sans text-6xl text-slate-300 font-bold col-start-2 col-span-2">
-						<i class="fa-solid fa-wrench"></i> TECH
+						<button hx-post="/clicked" hx-swap="outerHTML" class="bg-purple-700 h-max w-4/5 rounded-full font-sans text-4xl text-slate-300 font-bold col-start-2 col-span-2">
+							<p class="my-10"><i class="fa-solid fa-wrench"></i>TECH</p>
 						</button>
 					</div>
 				</body>
