a4b8083a0b7ae830bdc80d743cf52538f1fde490
CAMPB3LL <93111643+CAMPB3LL@users.noreply.github.com>
2023-11-25 12:42:11 +0930

    Complete Overhaul
    Following new figma design

--- index.tsx
+++ index.tsx
@@ -7,22 +7,32 @@ const app = new Elysia()
 	.get('/', ({ html }) =>
 		html(
 			<BaseHtml>
-				<body class="flex w-full h-screen justify-center items-center text-slate-300 font-sans text-4xl font-bold antialiased">
-					<div id="register" hx-swap="outerHTML swap:0.25s" class="fade-me-out w-full h-1/5 justify-items-center grid grid-cols-2 gap-y-40 m-20">
-						<p class="font-sans text-7xl text-slate-300 font-bold col-start-1 col-span-2">Who are you?</p>
-						<button hx-get="/user" hx-trigger="click" hx-target="#register" class="bg-purple-600 h-auto w-4/5 rounded-full col-start-1 col-span-1">
-							<p class="my-10"><i class="fa-solid fa-user"></i>USER</p>
-						</button>
-						<button hx-get="/tech" hx-trigger="click" hx-target="#register" class="bg-purple-700 h-auto w-4/5 rounded-full col-start-2 col-span-2">
-							<p class="my-10"><i class="fa-solid fa-wrench"></i>TECH</p>
-						</button>
-					</div>
+				<body class="flex flex-col space-y-16 p-24 w-full h-screen text-slate-300 font-bold antialiased">
+
+					{/* <p class="font-sans text-7xl text-slate-300 font-bold col-start-1 col-span-2">Who are you?</p>
+					<button hx-get="/user" hx-trigger="click" hx-target="#register" class="bg-gradient-to-r from-cyan-500 to-blue-500  h-auto w-4/5 rounded-full col-start-1 col-span-1">
+						<p class="my-10"><i class="fa-solid fa-user"></i>USER</p>
+					</button>
+
+					<button hx-get="/tech" hx-trigger="click" hx-target="#register" class="bg-gradient-to-r from-cyan-500 to-blue-500  h-auto w-4/5 rounded-full col-start-2 col-span-2">
+						<p class="my-10"><i class="fa-solid fa-wrench"></i>TECH</p>
+					</button>
+
+					<button class="g-signin2 bg-gradient-to-r from-cyan-500 to-blue-500  h-auto w-4/5 rounded-full col-start-1 col-span-1" data-onsuccess="onSignIn"></button>
+
+					<button class="g-signin2 bg-gradient-to-r from-cyan-500 to-blue-500  h-auto w-4/5 rounded-full col-start-2 col-span-2" data-onsuccess="onSignIn"></button> */}
+
+					<h1 class="text-8xl italic font-extrabold">TechSwift</h1>
+					<h2 class="text-6xl italic">Providing On-Demand IT Services to You!</h2>
+					<button class="g-signin2 rounded-full" data-onsuccess="onSignIn"></button>
+
 				</body>
 			</BaseHtml>
 		)
 	)
 	.get("/tech", () =>
 	<div id="tech-register" hx-swap="outerHTML settle:0.5s" class="fade-me-in w-full h-screen justify-items-center grid grid-cols-2 m-20 py-40">
+
 		<p class="font-sans text-slate-300 font-bold col-start-1 col-span-2 my-auto text-7xl">Register</p>
 		<input class="text-center rounded-full h-3/4 w-full col-start-1 col-span-2" type="text" id="fname" name="fname" placeholder="First Name"></input>
 		<input class="text-center rounded-full h-3/4 w-full col-start-1 col-span-2" type="text" id="lname" name="lname" placeholder="Last Name"></input>
@@ -30,10 +40,10 @@ const app = new Elysia()
 		<input class="text-center rounded-full h-3/4 w-full col-start-1 col-span-2" type="text" id="password" name="password" placeholder="Password"></input>
 		<input class="text-center rounded-full h-3/4 w-full col-start-1 col-span-2" type="text" id="cpassword" name="cpassword" placeholder="Confirm Password"></input>
 
-		<button hx-get="/" hx-trigger="click" class="bg-purple-600 h-3/5 w-4/5 rounded-full col-start-1 col-span-1">
+		<button hx-get="/" hx-trigger="click" class="bg-gradient-to-r from-cyan-500 to-blue-500  h-3/5 w-4/5 rounded-full col-start-1 col-span-1">
 			<p class="my-10">Back</p>
 		</button>
-		<button hx-get="/submit" hx-trigger="click" class="bg-purple-700 h-3/5 w-4/5 rounded-full col-start-2 col-span-2">
+		<button hx-get="/submit" hx-trigger="click" class="bg-gradient-to-r from-cyan-500 to-blue-500  h-3/5 w-4/5 rounded-full col-start-2 col-span-2">
 			<p class="my-10">Submit</p>
 		</button>
 
@@ -48,10 +58,10 @@ const app = new Elysia()
 		<input class="text-center rounded-full h-3/4 w-full col-start-1 col-span-2" type="text" id="password" name="password" placeholder="Password"></input>
 		<input class="text-center rounded-full h-3/4 w-full col-start-1 col-span-2" type="text" id="cpassword" name="cpassword" placeholder="Confirm Password"></input>
 
-		<button hx-get="/" hx-trigger="click" class="bg-purple-600 h-3/5 w-4/5 rounded-full col-start-1 col-span-1">
+		<button hx-get="/" hx-trigger="click" class="bg-gradient-to-r from-cyan-500 to-blue-500  h-3/5 w-4/5 rounded-full col-start-1 col-span-1">
 			<p class="my-10">Back</p>
 		</button>
-		<button hx-get="/submit" hx-trigger="click" class="bg-purple-700 h-3/5 w-4/5 rounded-full col-start-2 col-span-2">
+		<button hx-get="/submit" hx-trigger="click" class="bg-gradient-to-r from-cyan-500 to-blue-500  h-3/5 w-4/5 rounded-full col-start-2 col-span-2">
 			<p class="my-10">Submit</p>
 		</button>
 
@@ -71,22 +81,13 @@ const BaseHtml = ({children}: elements.Children) =>
 <script src="https://unpkg.com/htmx.org@1.9.7"></script>
 <script src="https://cdn.tailwindcss.com"></script>
 <script src="https://kit.fontawesome.com/0902ff1ba2.js" crossorigin="anonymous"></script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<meta name="google-signin-client_id" content="668696165605-lfsea9h6h6hl3hoofbdhp7k6cmli87rl.apps.googleusercontent.com">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+<style>body {font-family: 'Roboto', sans-serif;}</style>
 </head>
-
-<style>
-.fade-me-out.htmx-swapping {
-	opacity: 0;
-	transition: opacity 0.25s ease-out;
-}
-
-.fade-me-in.htmx-added {
-	opacity: 0;
-}
-.fade-me-in {
-	opacity: 1;
-	transition: opacity 0.5s ease-out;
-}
-</style>
 
 ${children}`
 ;
